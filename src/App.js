e1f1e4758e31a6cfbdd1cd4a3252645cc75ba688
Semir <sblekic1@etf.unsa.ba>
2022-07-23 21:01:08 +0200

    React router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   
   return (
-    <div className="App">
-      <Navbar />
-     <div className="content">
-      <Home />
-     </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+      <div className="content">
+        <Switch>
+          <Route path="/">
+            {/* u switch navodimo Route a u njemu path na koji ce se pozvati komponenta Home, u ovom primjeru */}
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+      </div>
+    </Router>
   );
 }
 
