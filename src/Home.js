baea5cb7006ae9a20b7bca1fcb34f0952cac3997
Semir <sblekic1@etf.unsa.ba>
2022-07-23 17:47:14 +0200

    added little loading message while fetching data

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,25 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const[isPending, setIsPending] = useState(true);
 
     useEffect(() => { 
+       setTimeout(() => {  {/* just simaluting request taking a bit longer */}
         fetch('http://localhost:8000/blogs').then(res => {
             return res.json(); //this is also async
         }).then((data) => {
             setBlogs(data);
            // console.log(data[0].body);
+           setIsPending(false);
         })
+       }, 1000);
     }, []);
 
     return (
         <div className="home">
+            { isPending && <div>Loading...</div> }
             { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
-            {/*da nije bilo ove provjere, blogs array bi bio prazan */}
+            {/*da nije bilo ove provjere, blogs array bi bio prazan i ne bi imali sta proslijediti */}
         </div>
     );
 }
