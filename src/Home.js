d1ecd917655efb7db798555a6044d7056f30743d
Semir <sblekic1@etf.unsa.ba>
2022-07-23 16:40:26 +0200

    added json server for future fetching data

--- src/Home.js
+++ src/Home.js
@@ -8,24 +8,19 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
-    const [name, setName] = useState('mario');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-    useEffect(() => { {/* ova funkcija se poziva na svaki render i izmijenu podataka(blogs sa setBlogs npr) */}
+    useEffect(() => { 
         console.log('use effect ran');
-    }, [name]); {/*but if we put dependency array, it will fire up only on first render
- */}   {/* u array stavljamo ono sto zelimo da bude "ovisno" tj. useEffect state ce se pozvati kad se promijeni ono sto je u dependency array-u*/}
+    }, []);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>  {/* function as a prop */}
-            {/* <BlogList blogs={blogs.filter((blog) => (blog.author === 'mario'))} title="Mario's blogs" /> */}
-            <button onClick={() => setName('luigi')}>Change name</button>
-            <p> {name} </p>
+            
         </div>
     );
 }
